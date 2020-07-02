f6330aaf05f03833bdaa09d43b713cbcfb371385
jhonrmelo <45439356+jhonrmelo@users.noreply.github.com>
2020-07-01 22:48:27 -0300

    Initial commit

---
+++ LojaVirtual/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace LojaVirtual.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public string Description { get; set; }
+    }
+}
